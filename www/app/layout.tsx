cabdea9d7b61cfb372dd9903d17c865eaaeb8e7c
Chris Nielsen <nielsen.chris@gmail.com>
2026-04-10 19:49:07 -0400

    Header infrastructure

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { ReactNode } from 'react';
+import Header from '../components/header/Header';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<Header />
+				{children}
+			</body>
 		</html>
 	);
 }
