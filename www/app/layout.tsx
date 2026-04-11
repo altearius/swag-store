41cad67fa1a53eb8b3b6e0b7f981207eb1813494
Chris Nielsen <nielsen.chris@gmail.com>
2026-04-10 21:38:33 -0400

    Add some layout metadata.

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
@@ -17,3 +18,20 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 		</html>
 	);
 }
+
+export const metadata: Metadata = {
+	title: 'Swag Store',
+	description: 'A store for swag',
+	openGraph: {
+		title: 'Swag Store',
+		description: 'A store for swag',
+		siteName: 'Swag Store',
+		type: 'website',
+	},
+	robots: {
+		// Assuming Vercel would rather this example site is not indexed by
+		// search engines.
+		index: false,
+		follow: false,
+	},
+};
