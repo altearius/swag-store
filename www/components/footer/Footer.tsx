bbd869d820c21b6485ab14208064a9624d5c5c03
Chris Nielsen <nielsen.chris@gmail.com>
2026-04-10 21:54:32 -0400

    lil note

--- www/components/footer/Footer.tsx
+++ www/components/footer/Footer.tsx
@@ -1,6 +1,11 @@
 import styles from './Footer.module.css';
 
 export default function Footer() {
+	// 'new Date()' is non-deterministic and would typically not play well with
+	// a cache. However, since we only use the year value, which only changes
+	// once a year, I judge that dealing with the stale year value is preferable
+	// than the complication that would come with treating this
+	// as non-deterministic.
 	const year = new Date().getFullYear();
 
 	return <footer className={styles['footer']}>© {year} Swag Store</footer>;
