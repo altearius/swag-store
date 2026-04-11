35fc4137f9c854ec96dee13d7ff10e4c641df727
Chris Nielsen <nielsen.chris@gmail.com>
2026-04-11 00:06:55 -0400

    This is probably cachable.

--- www/api/store/getStoreConfiguration.ts
+++ www/api/store/getStoreConfiguration.ts
@@ -1,6 +1,9 @@
+import { cacheLife } from 'next/cache';
 import createClient from '../createClient';
 
 export default async function getStoreConfiguration() {
+	'use cache';
+	cacheLife('hours');
 	const client = createClient();
 	const result = await client.GET('/store/config');
 	return result.data?.data ?? null;
