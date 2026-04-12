2e6b6f4df18c4e72af50c5494a787f234eb6c527
Chris Nielsen <nielsen.chris@gmail.com>
2026-04-12 11:59:35 -0400

    Working on cart badge, time for lunch

--- www/components/header/cart/Cart.tsx
+++ www/components/header/cart/Cart.tsx
@@ -1,7 +1,18 @@
+'use client';
+
+import useCart from '../../../cart/useCart';
+
 interface Props {
 	readonly className?: string | undefined;
 }
 
 export default function Cart(p: Props) {
-	return <span className={p.className}>🛒</span>;
+	const { contents } = useCart();
+	const itemCount = contents?.totalItems ?? 0;
+
+	return (
+		<span className={p.className}>
+			🛒{itemCount > 0 ? <span className="item-count">{itemCount}</span> : null}
+		</span>
+	);
 }
