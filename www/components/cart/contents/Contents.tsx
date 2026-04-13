5b876d9fbe79e5b1370aa877881be5abbe8efef8
Chris Nielsen <nielsen.chris@gmail.com>
2026-04-12 20:58:23 -0400

    Sort this list

--- www/components/cart/contents/Contents.tsx
+++ www/components/cart/contents/Contents.tsx
@@ -1,3 +1,4 @@
+import type { CartItem } from '#api/api.types';
 import useCart from '../../../cart/useCart';
 import Item from '../item/Item';
 import style from './Contents.module.css';
@@ -13,13 +14,41 @@ export default function Contents() {
 		return <p>Your cart is empty.</p>;
 	}
 
+	const sortedItems = [...contents.items].sort(sortItems);
+
 	return (
 		<ul className={style['contents']}>
-			{contents.items.map((item, idx) => (
+			{sortedItems.map((item, idx) => (
 				<li key={item.productId ?? idx}>
 					<Item item={item} />
 				</li>
 			))}
 		</ul>
 	);
+}
+
+function sortItems(a: CartItem, b: CartItem) {
+	const addedAtA = a.addedAt?.valueOf() ?? 0;
+	const addedAtB = b.addedAt?.valueOf() ?? 0;
+
+	if (addedAtA < addedAtB) {
+		return -1;
+	}
+
+	if (addedAtA > addedAtB) {
+		return 1;
+	}
+
+	const productIdA = a.productId ?? a.product?.id ?? '';
+	const productIdB = b.productId ?? b.product?.id ?? '';
+
+	if (productIdA < productIdB) {
+		return -1;
+	}
+
+	if (productIdA > productIdB) {
+		return 1;
+	}
+
+	return 0;
 }
