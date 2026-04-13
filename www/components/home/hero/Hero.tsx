72bc557f110a7f0743c27cbfeb5a5ac8f0a2e4a3
Chris Nielsen <nielsen.chris@gmail.com>
2026-04-13 12:01:08 -0400

    Hero is supposed to have a visual element

--- www/components/home/hero/Hero.tsx
+++ www/components/home/hero/Hero.tsx
@@ -1,37 +1,37 @@
-import clsx from 'clsx';
 import Link from 'next/link';
+import SwagCat from '../swag-cat/SwagCat';
 import styles from './Hero.module.css';
-
-// TODO: Docs say this is supposed to have a visual element.
-// I'm waiting until I get some products in the store, maybe I'll pick a
-// product image or something.
 
 export default function Hero() {
 	return (
-		<section className={clsx(styles['hero'], 'layout-max-width')}>
-			<h1>Welcome to the Swag Store</h1>
+		<section className={styles['hero']}>
+			<h1 className="layout-max-width">Welcome to the Swag Store</h1>
 
-			<p>
-				<dfn>
-					<abbr title="Stolen Without a Gun">SWAG</abbr>
-				</dfn>{' '}
-				refers to merchandise which has been obtained through nefarious means,
-				such as theft. Another common usage refers to promotional items that are
-				given away for free.
-			</p>
+			<SwagCat />
 
-			<p>
-				This items have price tags on them, so they aren't free. You may
-				therefore assume that these items were originally stolen and are now
-				being sold for a profit. We don't ask questions about where the items
-				came from, we just sell them.
-			</p>
+			<div className="layout-max-width">
+				<p>
+					<dfn>
+						<abbr title="Stolen Without a Gun">SWAG</abbr>
+					</dfn>{' '}
+					refers to merchandise which has been obtained through nefarious means,
+					such as theft. Another common usage refers to promotional items that
+					are given away for free.
+				</p>
 
-			<p>
-				<Link href="/search" className="button">
-					Shop Now
-				</Link>
-			</p>
+				<p>
+					This items have price tags on them, so they aren't free. You may
+					therefore assume that these items were originally stolen and are now
+					being sold for a profit. We don't ask questions about where the items
+					came from, we just sell them.
+				</p>
+
+				<p>
+					<Link href="/search" className="button">
+						Shop Now
+					</Link>
+				</p>
+			</div>
 		</section>
 	);
 }
---
+++ www/components/home/swag-cat/SwagCat.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import robbery from './robber-robbery.gif';
+import style from './SwagCat.module.css';
+
+export default function SwagCat() {
+	return (
+		<div className={style['swag-cat']}>
+			<Image src={robbery} alt="lil cat doing a swag" />
+		</div>
+	);
+}
