6cf16b57d85b043485981366c6960a0b16a8f390
Chris Nielsen <nielsen.chris@gmail.com>
2026-04-13 09:05:54 -0400

    Client-side navigation for search controls.

--- www/components/search/controls/Controls.tsx
+++ www/components/search/controls/Controls.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import type { Categories as CategoryList } from '#api/api.types';
-import { useRef } from 'react';
+import { useRouter } from 'next/navigation';
+import { useCallback, useRef } from 'react';
 import Categories from '../categories/Categories';
 import Input from '../input/Input';
 import style from './Controls.module.css';
@@ -12,9 +13,39 @@ interface Props {
 
 export default function Controls(p: Props) {
 	const formRef = useRef<HTMLFormElement>(null);
+	const router = useRouter();
+
+	const action = useCallback((formData: FormData) => {
+		const search = formData.get('search');
+		const category = formData.get('category');
+		const params = new URLSearchParams();
+
+		if (typeof search === 'string') {
+			const parsed = search.trim();
+
+			if (parsed) {
+				params.set('search', parsed);
+			}
+		}
+
+		if (typeof category === 'string') {
+			const parsed = category.trim();
+
+			if (parsed) {
+				params.set('category', parsed);
+			}
+		}
+
+		router.push(`?${params.toString()}`);
+	}, []);
 
 	return (
-		<form className={style['controls']} role="search" ref={formRef}>
+		<form
+			className={style['controls']}
+			role="search"
+			ref={formRef}
+			action={action}
+		>
 			<p>
 				<Input />
 			</p>
