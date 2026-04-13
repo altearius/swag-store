74c62b2da4b977e51ad8b48353843e476aeac606
Chris Nielsen <nielsen.chris@gmail.com>
2026-04-12 23:56:54 -0400

    just realized categories is a single-select thing.

--- www/components/search/controls/Controls.tsx
+++ www/components/search/controls/Controls.tsx
@@ -1,10 +1,7 @@
 'use client';
 
 import type { Categories } from '#api/api.types';
-import type { ChangeEventHandler } from 'react';
-import { useCallback, useRef, useState } from 'react';
-import type { Handle } from '../categories/Modal';
-import Modal from '../categories/Modal';
+import { Suspense, use, useRef } from 'react';
 import style from './Controls.module.css';
 
 interface Props {
@@ -12,39 +9,54 @@ interface Props {
 }
 
 export default function Controls(p: Props) {
-	const [categoryCount, setCategoryCount] = useState(0);
-	const modalRef = useRef<Handle>(null);
 	const formRef = useRef<HTMLFormElement>(null);
-	const noun = categoryCount === 1 ? 'category' : 'categories';
-
-	const onClickCategories = useCallback(() => {
-		modalRef.current?.open();
-	}, []);
-
-	const onChange = useCallback<ChangeEventHandler>(() => {
-		const form = formRef.current;
-		setCategoryCount(form ? new FormData(form).getAll('categories').length : 0);
-	}, []);
 
 	return (
-		<form
-			className={style['controls']}
-			role="search"
-			onChange={onChange}
-			ref={formRef}
-		>
+		<form className={style['controls']} role="search" ref={formRef}>
 			<p>
 				<label>
 					<input type="text" name="search" placeholder="🔎 Search" />
 				</label>{' '}
+				<Suspense fallback={<CategorySkeleton />}>
+					<CategoryDropdown categories={p.categories} />
+				</Suspense>{' '}
 				<button type="submit">Go</button>{' '}
 			</p>
-			<p>
-				<button type="button" onClick={onClickCategories}>
-					⚙️ {categoryCount.toLocaleString()} {noun} selected
-				</button>{' '}
-			</p>
-			<Modal categories={p.categories} ref={modalRef} />{' '}
 		</form>
+	);
+}
+
+function CategorySkeleton() {
+	return (
+		<select disabled>
+			<option>- loading -</option>
+		</select>
+	);
+}
+
+function CategoryDropdown(p: Props) {
+	const categories = use(p.categories);
+
+	if (categories.size === 0) {
+		return (
+			<select disabled>
+				<option>No categories found.</option>
+			</select>
+		);
+	}
+
+	const sorted = Array.from(categories).sort(([, a], [, b]) =>
+		(a.name ?? '').localeCompare(b.name ?? ''),
+	);
+
+	return (
+		<select name="category">
+			<option value="">All categories</option>
+			{sorted.map(([slug, { name }]) => (
+				<option key={slug} value={slug}>
+					{name}
+				</option>
+			))}
+		</select>
 	);
 }
